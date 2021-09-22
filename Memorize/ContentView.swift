8fe638c233f9d1c6d34ad9f9f572df9b66027dd0
AntoninB84 <antonin.billot@gmail.com>
2021-09-22 11:44:39 +0200

    Cartes clicables

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,21 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 20)
-                .strokeBorder(lineWidth: 3, antialiased: true)
-                
-            Text("Hello")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView(isFaceUp: false)
         }
-        .foregroundColor(.orange)
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
+            
+    }
+}
+
+struct CardView : View{
+    
+    // Le mot clé @State permet de dire que la variable a une durée de vie plus longue que celle de la vue
+    // et peut donc être utilisée plus longtemps que lors de la création de la vue
+    @State var isFaceUp = true
+    
+    var shape = RoundedRectangle(cornerRadius: 20)
+    
+    var body : some View {
+        ZStack{
+            if isFaceUp{
+                
+                shape.fill(Color.yellow)
+                
+                shape.strokeBorder(lineWidth: 5, antialiased: true)
+                
+                Text("🚗")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+            }else{
+                shape
+            }
+        }
+        .foregroundColor(.red)
+        .padding()
+        .onTapGesture(perform: {
+            isFaceUp = !isFaceUp
+        })
     }
 }
